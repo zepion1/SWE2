3193d1e2dacfeddae227ac263e4fa09c365c1db5
GuyNamedMarc <marclabib324@gmail.com>
2025-04-03 12:26:02 -0400

    Fixed the title on the calendar tab

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 const CalendarPage = () => {
+    useEffect(() => {
+                document.title = "Calendar | MSU Companion";
+            }, []);
     return (
         <div className="calendar-page">
             <h1 className="calendar-title">Calendar (WORK IN PROGRESS)</h1>
