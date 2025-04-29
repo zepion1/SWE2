d9d9afa9ebf9269e8ee25abaf94d17b0e3e31b83
OG247 <rafaelbernardo2407@gmail.com>
2025-04-28 21:29:21 -0400

    Calendar finally fixed
    it was a pain to style but i finally did it

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 const CalendarPage = () => {
     useEffect(() => {
-                document.title = "Calendar | MSU Companion";
-            }, []);
+        document.title = "Calendar | MSU Companion";
+    }, []);
+
     return (
         <div className="calendar-page">
-            <h1 className="calendar-title">Calendar (WORK IN PROGRESS)</h1>
+            <h1 className="calendar-title">Calendar</h1>
+            <p className="calendar-subtitle">Plan your schedule with ease!</p>
             <Calendar className="react-calendar" locale="en-US" />
         </div>
     );
